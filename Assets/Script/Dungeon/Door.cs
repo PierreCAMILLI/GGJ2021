89a8f34cd158e14a2a13c84fb779824475a8a4b8
Marc Gavanier <marc.gavanier@gmail.com>
2021-01-30 15:17:36 +0100

    refactor room manager, room and door script to simplify prefab usage

--- Assets/Script/Dungeon/Door.cs
+++ Assets/Script/Dungeon/Door.cs
@@ -5,24 +5,19 @@ namespace Dungeon
 {
     public class Door : MonoBehaviour
     {
-        [SerializeField] private RoomManager RoomManager;
-
         [SerializeField] private Room Room;
 
         [SerializeField] private Door NextRoomDoor;
 
         [SerializeField] private GameObject In;
 
-        [SerializeField] private UnityEvent RoomChanged;
-
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.gameObject.CompareTag("Player")) return;
 
-            RoomManager.ChangeRoom(NextRoomDoor.Room);
             other.transform.position = NextRoomDoor.In.transform.position;
 
-            RoomChanged.Invoke();
+            Room.RoomManager.ChangeRoom(NextRoomDoor.Room);
         }
     }
 }
