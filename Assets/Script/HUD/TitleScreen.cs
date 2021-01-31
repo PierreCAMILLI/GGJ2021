967ab2175039fa69eb019b2e4c33accffd3a5732
Marc Gavanier <marc.gavanier@gmail.com>
2021-01-31 02:00:03 +0100

    use play on awake from audio listner instead of method in title screen script

--- Assets/Script/HUD/TitleScreen.cs
+++ Assets/Script/HUD/TitleScreen.cs
@@ -1,17 +1,7 @@
-
-
-using Audio;
 using UnityEngine;
 
 public class TitleScreen : SingletonBehaviour<TitleScreen>
 {
-    [SerializeField] private AudioManager AudioManager;
-
-    public void Start()
-    {
-        AudioManager.PlayMenuMusic();
-    }
-
     public void StartGame()
     {
         GlobalEvents.Instance.EventStartTuto.Invoke();
